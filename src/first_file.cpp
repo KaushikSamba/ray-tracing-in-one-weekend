7df3b04c062e295b54c7519991b47712f715206e
Kaushik Sambamurthy <kaushik.s02@gmail.com>
2022-10-28 22:23:36 -0400

    Print out a PPM file
    - This completes Chapter 1

--- src/first_file.cpp
+++ src/first_file.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <fmt/core.h>
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    const int NUM_COLS = 200;
+    const int NUM_ROWS = 100;
+    const float SCALING_VALUE = 254.99;
+
+    std::cout << fmt::format ("P3\n{} {}\n255\n", NUM_COLS, NUM_ROWS) ;
+
+    for(int j = NUM_ROWS-1; j >=0; j--)
+    {
+        for(int i = 0; i < NUM_COLS; i++)
+        {
+            float r = static_cast<float>(i)/NUM_COLS;
+            float g = static_cast<float>(j)/NUM_ROWS;
+            float b = 0.2;
+
+            int ir = static_cast<int>(SCALING_VALUE * r);
+            int ig = static_cast<int>(SCALING_VALUE * g);
+            int ib = static_cast<int>(SCALING_VALUE * b);
+
+            std::cout << fmt::format("{} {} {} ", ir, ig, ib);
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
