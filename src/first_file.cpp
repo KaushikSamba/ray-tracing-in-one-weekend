fecdf9bf7f41c7e3584df994c14acca4e749d3ea
Kaushik Sambamurthy <kaushik.sambamurthy@alertinnovation.com>
2023-04-28 16:19:17 -0400

    Use color class for PPM image creation

--- src/first_file.cpp
+++ src/first_file.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <fmt/core.h>
+#include "color.hpp"
 
 int main()
 {
-    const int   NUM_COLS      = 200;
-    const int   NUM_ROWS      = 100;
-    const float SCALING_VALUE = 254.99;
+    const int NUM_COLS = 256;
+    const int NUM_ROWS = 256;
 
     std::cout << fmt::format("P3\n{} {}\n255\n", NUM_COLS, NUM_ROWS);
 
@@ -14,17 +14,13 @@ int main()
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for(int i = 0; i < NUM_COLS; i++)
         {
-            float r = static_cast<float>(i) / NUM_COLS;
-            float g = static_cast<float>(j) / NUM_ROWS;
-            float b = 0.2;
+            float r = static_cast<float>(i) / (NUM_COLS - 1);
+            float g = static_cast<float>(j) / (NUM_ROWS - 1);
+            float b = 0.25;
 
-            int ir = static_cast<int>(SCALING_VALUE * r);
-            int ig = static_cast<int>(SCALING_VALUE * g);
-            int ib = static_cast<int>(SCALING_VALUE * b);
-
-            std::cout << fmt::format("{} {} {} ", ir, ig, ib);
+            Color color {r, g, b};
+            write_color(std::cout, color);
         }
-        std::cout << std::endl;
     }
 
     std::cerr << "\nDone";
