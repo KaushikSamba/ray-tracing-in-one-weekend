5af6bdea2b70d7f81b799352962ff0fdfa0af274
Kaushik Sambamurthy <kaushik.sambamurthy@alertinnovation.com>
2023-04-28 16:06:32 -0400

    Apply clang-format to file

--- src/first_file.cpp
+++ src/first_file.cpp
@@ -3,19 +3,19 @@
 
 int main()
 {
-    const int NUM_COLS = 200;
-    const int NUM_ROWS = 100;
+    const int   NUM_COLS      = 200;
+    const int   NUM_ROWS      = 100;
     const float SCALING_VALUE = 254.99;
 
-    std::cout << fmt::format ("P3\n{} {}\n255\n", NUM_COLS, NUM_ROWS) ;
+    std::cout << fmt::format("P3\n{} {}\n255\n", NUM_COLS, NUM_ROWS);
 
-    for(int j = NUM_ROWS-1; j >=0; j--)
+    for(int j = NUM_ROWS - 1; j >= 0; j--)
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for(int i = 0; i < NUM_COLS; i++)
         {
-            float r = static_cast<float>(i)/NUM_COLS;
-            float g = static_cast<float>(j)/NUM_ROWS;
+            float r = static_cast<float>(i) / NUM_COLS;
+            float g = static_cast<float>(j) / NUM_ROWS;
             float b = 0.2;
 
             int ir = static_cast<int>(SCALING_VALUE * r);
